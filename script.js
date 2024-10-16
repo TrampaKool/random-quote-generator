12b50cd2d2dba22f08636af889d479317624558a
TrampaKool <labismail25@gmail.com>
2024-10-16 14:18:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,9 +52,7 @@ const QuoteWindow = () => {
     React.createElement(React.Fragment, null, /*#__PURE__*/
     React.createElement("p", {
       className:
-      "fw-bold text-transition fs-4" + (loading ? "fade-out" : "fade-in") },
-
-
+      "fw-bold text-transition fs-4 " + (loading ? "fade-out" : "fade-in") },
     quote), /*#__PURE__*/
 
     React.createElement("p", { className: "text-transition " + (loading ? "fade-out" : "fade-in") },
